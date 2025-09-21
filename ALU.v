6d89afd4549f9b55ea05d44cbe3867c94cc05f54
Gulaba  Ram <gulabgkg99@gmail.com>
2025-09-21 11:08:48 +0530

    ALU designing complete

--- ALU.v
+++ ALU.v
@@ -1,7 +1,8 @@
-module (A,B,ALUcontrol,Result);
+module (A,B,ALUcontrol,Result,Z,V,N,C);
 //input declare
  input [31:0] A,B;
  input [2:0] ALUcontrol;
+ input Z,V,N,C; //flags
 
  ///output declare
  output [31:0] Result;
@@ -10,6 +11,7 @@ module (A,B,ALUcontrol,Result);
  wire [31:0] a_&_b;
  wire [31:0] a_or_b;
  wire [31:0] not_b;
+ wire cout; //carry out
 
  //mux1
  wire [31:0] mux_1;
@@ -30,13 +32,23 @@ module (A,B,ALUcontrol,Result);
  assign mux_1 = (ALUcontrol[0] == 1'b0) ? B : not_b;
 
  //additonand subtraction operation
-    assign sum = A + mux_1 + ALUcontrol[0];
+ assign {cout,sum } = A + mux_1 + ALUcontrol[0]; /// use of carry out using concating property 
 
  //assign mux2
  assign mux_2 = (ALUcontrol[1:0] == 2'b00) ? sum : 
                 (ALUcontrol[1:0] == 2'b01) ? sum :
                  (ALUcontrol[1:0] == 2'b10) ? a_&_b : a_or_b;  
  //assign result 
- assign Result = mux_2;                 
-    
+ assign Result = mux_2;    
+
+//=================flag assignment==//
+
+assign Z= &{~Result}; //zero flag
+
+assign N= Result[31];  //negative flag if first bit is 1 then result is negative
+
+assign C= cout & (~ALUcontrol[1]); //carry out flag  
+
+assign V= (~ALUcontrol[1]) & (A[31] ^ sum[31]) & (~(A[31] ^ B[31]^ALUcontrol[0])); //overflow flag
+
 endmodule
